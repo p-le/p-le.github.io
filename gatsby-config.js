078da986619fa72ebb0d74d158ee4cdf4d23c790
p-le <phu.le.dev@gmail.com>
2022-03-21 01:10:08 +0000

    add github workflow

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `new`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://fullstackage.phu-le.dev`
   },
   plugins: ["gatsby-plugin-postcss", {
     resolve: 'gatsby-plugin-google-analytics',
@@ -11,7 +11,7 @@ module.exports = {
   }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png"
+      "icon": "src/images/logo.png"
     }
   }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
---
+++ static/README.md
@@ -0,0 +1,6 @@
+<h1 align="center">
+  <a href="https://www.youtube.com/c/FullstacKAGE">FullstacKAGE Service Hub</a>
+</h1>
+
+## 🚀 Notes
+Output files was built from `main` branch using GatsbyJS
